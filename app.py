fa01f1b6dab46e9c95618b7b49a603c1e1e896f2
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2015-11-28 22:29:53 -0600

    feat: Sorting categories

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, jsonify, session as login_session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from models import Base, Category, User, Item
 
@@ -14,14 +14,15 @@ db_session = DBSession()
 
 
 @app.route('/')
+@app.route('/categories/')
 def index():
-    categories = db_session.query(Category).all()
+    categories = db_session.query(Category).order_by(Category.name).all()
     return render_template('index.html', categories=categories, logged_in=False)
 
 
 @app.route('/categories/json/')
 def categories_json():
-    categories = db_session.query(Category).all()
+    categories = db_session.query(Category).order_by(Category.name).all()
     return jsonify(categories=[r.serialize for r in categories])
 
 
