df05410f65f07922d4b464c1bc5942f002666e6e
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2015-12-20 21:18:52 -0600

    refactor: Using submit  buttons included in form definition

--- appForms.py
+++ appForms.py
@@ -7,6 +7,7 @@ class EditItemForm(Form):
     name = StringField('Name', validators=[Length(min=3, message="Name too short"), DataRequired()])
     description = TextAreaField('Description')
     category_id = IntegerField('Category', validators=[DataRequired()])
+    save = SubmitField('Save')
 
 
 class DeleteItemForm(Form):
