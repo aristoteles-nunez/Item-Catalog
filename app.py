8e94d64afe6177a9e79ac6cefb3a99a8f188b126
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2016-01-01 23:01:08 -0600

    fix: Error messages in logout

--- app.py
+++ app.py
@@ -115,21 +115,12 @@ def gconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
-    access_token = login_session['access_token']
-    print('In gdisconnect access token is %s', access_token)
-    print('User name is: ')
-    print(login_session['username'].encode('utf-8'))
-    if access_token is None:
-        print('Access Token is None')
-        response = make_response(json.dumps('Current user not connected.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+    if 'access_token' not in login_session:
+        flash("Current user not connected.", "error")
+        return redirect(url_for('index'))
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print(url)
-    print('result is ')
-    print(result)
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
