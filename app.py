c800106ddcf2748d6e6b11fb1e3cb1b0544d5dc0
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2016-01-01 23:14:52 -0600

    fix: Error messages on login

--- app.py
+++ app.py
@@ -42,74 +42,48 @@ def delete_dir(dir_name):
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        flash("Invalid state parameter", "error")
+        return redirect(url_for('index'))
     code = request.data
-    print("first")
     try:
         oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
-        print('1.1')
         oauth_flow.redirect_uri = 'postmessage'
-        print('1.2')
         credentials = oauth_flow.step2_exchange(code.decode('utf-8'))
-        print('1.3')
     except FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    print("second")
-    print("2.1")
+        flash("Failed to upgrade the authorization code", "error")
+        return redirect(url_for('index'))
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}'.format(credentials.access_token))
     h = httplib2.Http()
-    print("2.2")
-    print(h.request(url, 'GET')[1])
     result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
-    print("2.3")
     if result.get('error') is not None:
-        response = make_response(json.dumps(result.get('error')), 505)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    print("third")
+        flash(result.get('error'), "error")
+        return redirect(url_for('index'))
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = make_response(json.dumps("Token's user id does not match given user "), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    print("forth")
+        flash("Token's user id does not match given user", "error")
+        return redirect(url_for('index'))
     if result['issued_to'] != CLIENT_ID:
-        response = make_response(json.dumps("Token's client id does not match"), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    print("fifth")
-
-    # Check to see if user is already logged in
+        flash("Token's client id does not match", "error")
+        return redirect(url_for('index'))
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps("Current user is already connected."), 200)
-        response.headers['Content-Type'] = 'application/json'
-    # Store the access tokens in the session for later use
-
+        flash("Current user is already connected.", "error")
+        return redirect(url_for('index'))
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
-
-    # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-    print('answer: {}'.format(answer.text))
     data = json.loads(answer.text)
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-
     user_id = get_user_id(login_session['email'])
     if not user_id:
         user_id = create_user()
     login_session['user_id'] = user_id
     flash("You are now logged in as %s" % login_session['username'])
-
     return "success"
 
 
