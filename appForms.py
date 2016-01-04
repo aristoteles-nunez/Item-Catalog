3642d101fa3d47fa13c46453eb68305b2989d833
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2016-01-04 01:49:17 -0600

    docs: Documenting each Form definition

--- appForms.py
+++ appForms.py
@@ -1,11 +1,14 @@
 from flask_wtf import Form
 from wtforms import SubmitField, StringField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired, Length
-from flask_wtf.file import FileRequired, FileAllowed, FileField
+from flask_wtf.file import FileAllowed, FileField
 
 
 class ItemForm(Form):
-    name = StringField('Name', validators=[Length(min=3, message="Name too short"), DataRequired()])
+    """It defines the fields to render in WTForms for each item
+    """
+    name = StringField('Name', validators=[Length(min=3, message="Name too short"),
+                                           DataRequired()])
     description = TextAreaField('Description')
     category_id = IntegerField('Category', validators=[DataRequired()])
     photo = FileField('Image for item', validators=[
@@ -14,9 +17,14 @@ class ItemForm(Form):
 
 
 class DeleteForm(Form):
+    """It defines the fields to delete items and categories
+    """
     delete = SubmitField('Delete')
 
 
 class CategoryForm(Form):
-    name = StringField('Name', validators=[Length(min=2, message="Name too short"), DataRequired()])
+    """It defines the fields to render in WTForms for each Category
+    """
+    name = StringField('Name', validators=[Length(min=2, message="Name too short"),
+                                           DataRequired()])
     save = SubmitField('Save')
