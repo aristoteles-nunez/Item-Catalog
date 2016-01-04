98baf33adce802ff96b62e3549116a54931591d0
Aristóteles Federico Núñez Juárez <aristoteles.nunez@gmail.com>
2016-01-04 01:41:58 -0600

    docs: Docummenting every method from app.py

--- app.py
+++ app.py
@@ -29,18 +29,32 @@ db_session = DBSession()
 
 
 def ensure_dir(file_name):
+    """ Method that ensure the existence of a directory that contains a file.
+        If the directory doesn't exist it will create one.
+    Args:
+        file_name: Name of the file that specify the path of the directory.
+    """
     dir_name = os.path.dirname(file_name)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
 
 def delete_dir(dir_name):
+    """ Method that deletes a directory only if it exists.
+    Args:
+        dir_name: Exactly path of the directory.
+    """
     if os.path.exists(dir_name):
         shutil.rmtree(dir_name)
 
 
 @app.route('/gdisconnect')
 def gdisconnect():
+    """ Route that makes the logout for Google Oauth account.
+
+    The user is disconnected deleting the information from current login_session, and
+     making a request to revoke the token emmited by Google Oauth
+    """
     if 'access_token' not in login_session:
         flash("Current user not connected.", "error")
         return redirect(url_for('index'))
@@ -62,6 +76,12 @@ def gdisconnect():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """ Route that makes the authentication for Google Oauth account.
+
+    Once the user authorizes the access to the his Google Oauth info,
+      it will be stored into the current login_session and if the user
+      does not exists it will be created and stored in database.
+    """
     if request.args.get('state') != login_session['state']:
         flash("Invalid state parameter", "error")
         return redirect(url_for('index'))
@@ -111,6 +131,17 @@ def gconnect():
 
 @app.route('/')
 def index():
+    """ Route that renders the index page.
+
+    Every time all the categories are obtained as well as the last 25 modified items.
+    The categories are sorted by name and  all the modified items are sorted by modified date.
+    The login_session will be passed to template to control the rendered elements
+     according to access rules.
+
+    Raises:
+        Exception. If something goes wrong, it will be display an error page with the proper
+         exception message.
+    """
     try:
         logged_in = 'username' in login_session
         categories = db_session.query(Category).order_by(Category.name).all()
@@ -129,6 +160,17 @@ def index():
 
 @app.route('/login/')
 def login():
+    """ Route that renders the login page.
+
+    It will be displaying all the login methods available, but for the moment only Google Oauth
+     will be displayed.
+    The CLIENT_ID as well as the STATE is passed to template from the server to dynamic render
+     on client side.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if logged_in:
@@ -147,16 +189,29 @@ def login():
 
 @app.route('/categories/<int:category_id>/items/')
 def get_category(category_id):
+    """ Route that renders the page for a specific category with the contained items.
+
+    All the contained items are sorted by name.
+    The login_session will be passed to template to control the rendered elements
+     according to access rules.
+
+    Args:
+        category_id: The id of the category to be displayed.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         categories = db_session.query(Category).order_by(Category.name).all()
-        categpry = db_session.query(Category).filter_by(id=category_id).one()
+        category = db_session.query(Category).filter_by(id=category_id).one()
         items = db_session.query(Item).filter_by(category_id=category_id)\
             .order_by(Item.name).all()
         return render_template('index.html', categories=categories,
                                active_category=int(category_id), items=items,
                                logged_in=logged_in, login_session=login_session,
-                               category_owner=categpry.user_id)
+                               category_owner=category.user_id)
     except Exception as e:
         flash('An error has occurred: {}'.format(str(e)), 'error')
         return redirect(url_for('index'))
@@ -164,6 +219,19 @@ def get_category(category_id):
 
 @app.route('/categories/<int:category_id>/items/<int:item_id>/')
 def get_item_by_category(category_id, item_id):
+    """ Route that renders the page for a specific item according to its category.
+
+    The login_session will be passed to template to control the rendered elements
+     according to access rules.
+
+    Args:
+        category_id: The id of the category of the item to be displayed.
+        item_id: The id of the item to be displayed.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         categories = db_session.query(Category).order_by(Category.name).all()
@@ -178,6 +246,21 @@ def get_item_by_category(category_id, item_id):
 
 @app.route('/categories/<int:category_id>/items/<int:item_id>/delete/', methods=['GET', 'POST'])
 def delete_item(category_id, item_id):
+    """ Route that renders the page to delete an item.
+
+    This method validate that the user is logged in, and the item were created by him, to avoid
+     malicious behaviors in the url.
+    The item is deleted from database and the folder created to store the uploaded images is deleted
+     as well.
+
+    Args:
+        category_id: The id of the category of the item to be deleted.
+        item_id: The id of the item to be deleted.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if not logged_in:
@@ -211,6 +294,22 @@ def delete_item(category_id, item_id):
 
 @app.route('/categories/<int:category_id>/items/<int:item_id>/edit/', methods=['GET', 'POST'])
 def edit_item(category_id, item_id):
+    """ Route that renders the page to edit an item.
+
+    This method validate that the user is logged in, and the item were created by him, to avoid
+     malicious behaviors in the url.
+    Every time the user uploads a new image, the image is stored in a folder that is named
+     with the item id.
+    Only one image path is stored in database.
+
+    Args:
+        category_id: The id of the category of the item to be edited.
+        item_id: The id of the item to be edited.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if not logged_in:
@@ -247,6 +346,18 @@ def edit_item(category_id, item_id):
 
 @app.route('/categories/<int:category_id>/items/new/', methods=['GET', 'POST'])
 def new_item(category_id):
+    """ Route that renders the page to add a new item.
+
+    This method validate that the user is logged in.
+    The item is associated with the current logged in user.
+
+    Args:
+        category_id: The id of the category of the item to be added.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if not logged_in:
@@ -283,6 +394,15 @@ def new_item(category_id):
 
 @app.route('/categories/new/', methods=['GET', 'POST'])
 def new_category():
+    """ Route that renders the page to add a new category.
+
+    This method validate that the user is logged in.
+    The category is associated with the current logged in user.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if not logged_in:
@@ -310,6 +430,18 @@ def new_category():
 
 @app.route('/categories/<int:category_id>/edit/', methods=['GET', 'POST'])
 def edit_category(category_id):
+    """ Route that renders the page to edit an category.
+
+    This method validate that the user is logged in, and the category were created by him, to avoid
+     malicious behaviors in the url.
+
+    Args:
+        category_id: The id of the category to be edited.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if not logged_in:
@@ -338,6 +470,22 @@ def edit_category(category_id):
 
 @app.route('/categories/<int:category_id>/delete/', methods=['GET', 'POST'])
 def delete_category(category_id):
+    """ Route that renders the page to delete a category.
+
+    This method validate that the user is logged in, and the category were created by him, to avoid
+     malicious behaviors in the url.
+    The category is deleted from database and all the items that belongs to this category are deleted
+     in cascade, as well as all the images associated with the items.
+    All the items are deleted even if weren't created for this user, it is enough to be the owner of
+     the category.
+
+    Args:
+        category_id: The id of the category to be deleted.
+
+    Raises:
+        If an error occurs the application will redirect to index page and a flash message
+        will be displayed with the proper Exception message.
+    """
     try:
         logged_in = 'username' in login_session
         if not logged_in:
@@ -368,6 +516,14 @@ def delete_category(category_id):
 
 @app.route('/json/categories/')
 def json_api_categories():
+    """ Route to render the page for JSON Api.
+
+    It will render all the information relative of the categories in a single response.
+
+    Raises:
+        If an error occurs the application will generate a JSON response
+        with the proper Exception message.
+    """
     try:
         categories = db_session.query(Category).order_by(Category.name).all()
         return jsonify(categories=[r.serialize for r in categories])
@@ -379,6 +535,17 @@ def json_api_categories():
 
 @app.route('/json/categories/<int:category_id>/items/')
 def json_api_get_category(category_id):
+    """ Route to render the page for JSON Api for a specified category.
+
+    It will render all the information relative to one category only.
+
+    Args:
+        category_id: The id of the category to be displayed.
+
+    Raises:
+        If an error occurs the application will generate a JSON response
+        with the proper Exception message.
+    """
     try:
         items = db_session.query(Item).filter_by(category_id=category_id).order_by(Item.name).all()
         return jsonify(items=[r.serialize for r in items])
@@ -390,6 +557,14 @@ def json_api_get_category(category_id):
 
 @app.route('/json/items/')
 def json_api_items():
+    """ Route to render the page for JSON Api for all the items.
+
+    It will render all the information relative of all items in a single response.
+
+    Raises:
+        If an error occurs the application will generate a JSON response
+        with the proper Exception message.
+    """
     try:
         items = db_session.query(Item).order_by(desc(Item.modified_date)).all()
         return jsonify(items=[r.serialize for r in items])
@@ -401,6 +576,17 @@ def json_api_items():
 
 @app.route('/json/items/<int:item_id>/')
 def json_api_get_item(item_id):
+    """ Route to render the page for JSON Api for a specified item.
+
+    It will render all the information relative to one item only.
+
+    Args:
+        item_id: The id of the item to be displayed.
+
+    Raises:
+        If an error occurs the application will generate a JSON response
+        with the proper Exception message.
+    """
     try:
         item = db_session.query(Item).filter_by(id=item_id).one()
         return jsonify(item.serialize)
@@ -412,6 +598,18 @@ def json_api_get_item(item_id):
 
 @app.route('/json/categories/<int:category_id>/items/<int:item_id>/')
 def json_api_get_item_by_category(category_id, item_id):
+    """ Route to render the page for JSON Api for a specified item.
+
+    It will render all the information relative to one item only.
+
+    Args:
+        item_id: The id of the item to be displayed.
+        category_id: The id of the category that contains the item to be displayed.
+
+    Raises:
+        If an error occurs the application will generate a JSON response
+        with the proper Exception message.
+    """
     try:
         item = db_session.query(Item).filter_by(id=item_id, category_id=category_id).one()
         return jsonify(item.serialize)
@@ -423,6 +621,14 @@ def json_api_get_item_by_category(category_id, item_id):
 
 @app.route('/xml/categories/')
 def xml_api_categories():
+    """ Route to render the page for XML Api.
+
+    It will render all the information relative of the categories in a single response.
+
+    Raises:
+        If an error occurs the application will generate a XML response
+        with the proper Exception message.
+    """
     try:
         categories = db_session.query(Category).order_by(Category.name).all()
         data = Element('categories')
@@ -454,11 +660,20 @@ def xml_api_categories():
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """ If a page is not found the application will redirect to index page and a flash message
+        will be displayed with an error Flash message.
+    """
     flash('Page not found: {}'.format(request.path), "error")
     return redirect(url_for('index'))
 
 
 def create_user():
+    """ Method that will create an user .
+    Returns:
+        user.id: The id of the created user
+    Raises:
+        If an error occurs it will be displayed in a error message.
+    """
     try:
         new_user = User(name=login_session['username'], email=login_session[
                        'email'], picture=login_session['picture'])
@@ -472,6 +687,15 @@ def create_user():
 
 
 def get_user_info(user_id):
+    """ Method that will get an user info.
+
+    Args:
+        user_id: The id of the user to be found.
+    Returns:
+        user: The user object from database
+    Raises:
+        If an error occurs it will be displayed in a error message.
+    """
     try:
         user = db_session.query(User).filter_by(id=user_id).one()
         return user
@@ -481,6 +705,15 @@ def get_user_info(user_id):
 
 
 def get_user_id(email):
+    """ Method that will get an user id from an email.
+
+    Args:
+        email: The email of the user to be found.
+    Returns:
+        user.id: The user id from database
+    Raises:
+        If an error occurs it will be displayed in a error message.
+    """
     try:
         user = db_session.query(User).filter_by(email=email).one()
         return user.id
